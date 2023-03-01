ccc768e3bbefe2487b08b08bb2577f2d04c0d0a5
manoj kumar <manojtewatia9211@gmail.com>
2023-03-01 13:08:45 +0530

    first commit

---
+++ src/data/achievementData.js
@@ -0,0 +1,42 @@
+import java from "../assets/Images/Java.png";
+import CSS from "../assets/Images/Css.png";
+import JsB from "../assets/Images/js.png";
+import Jsm from "../assets/Images/Javascript.png";
+
+export const achievementData = {
+  bio: "`Success is not final, failure is not fatal: it is the courage to continue that counts. - Winston Churchill.`",
+  achievements: [
+    {
+      id: 1,
+      title: "Hacker-Rank certificate for JAVA Basic",
+      details: "A Basic Java test by Hacker-Rank",
+      date: "Feb 26, 2023",
+      field: "DSA",
+      image: java,
+    },
+    {
+      id: 2,
+      title: "Hacker-Rank certificate for CSS Basic",
+      details: "A Basic CSS test by Hacker-Rank",
+      date: "Feb 26, 2023",
+      field: "Coding",
+      image: CSS,
+    },
+    {
+      id: 3,
+      title: "Hacker-Rank certificate for JAVASCRIPT Basic",
+      details: "A Basic JAVASCRIPT test by Hacker-Rank",
+      date: "Feb 26, 2023",
+      field: "Coding",
+      image: JsB,
+    },
+    {
+      id: 4,
+      title: "Hacker-Rank certificate for JAVASCRIPT Intermediate",
+      details: "A Intermediate JAVASCRIPT test by Hacker-Rank",
+      date: "Feb 28, 2021",
+      field: "Coding",
+      image: Jsm,
+    },
+  ],
+};
